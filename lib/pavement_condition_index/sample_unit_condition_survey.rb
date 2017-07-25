09144a930377cc551da72b808b978429248766c7
Andy Monroe <jayhawker805@gmail.com>
2017-07-25 17:12:03 -0500

    Implement all of sample_unit_condition_survey's necessary methods

--- lib/pavement_condition_index/sample_unit_condition_survey.rb
+++ lib/pavement_condition_index/sample_unit_condition_survey.rb
@@ -1,9 +1,7 @@
 module PavementConditionIndex
   class SampleUnitConditionSurvey
 
-    attr_reader :area
-    attr_reader :pavement_type
-    attr_reader :distresses
+    attr_reader :area, :pavement_type, :distresses
 
     def initialize(area:nil,pavement_type:nil,distresses:nil)
       @area = area
@@ -12,89 +10,60 @@ module PavementConditionIndex
     end
 
     def distress_groups
-      @distresses.group_by(&:type).each do |type, dist|
-        dist.group_by(&:severity).each do |severity, dist2|
-          DistressGroup.new(dist2)
+      groups = @distresses.group_by{|dst|
+        [dst.type,dst.severity]}.map{|key,value|
+          DistressGroup.new(type: key[0],
+                            severity: key[1],
+                            distresses: value,
+                            area: area,
+                            pavement_type: pavement_type)}
+    end
+
+    def cdv_iterations
+      @cdv_iterations ||= begin
+        cdv_iterations = [PavementConditionIndex::CdvIteration.new(deduct_values: allowed_deduct_values, pavement_type: @pavement_type)] # first row
+        while next_iteration_deduct_values = cdv_iterations.last.next_iteration_deduct_values # while next_iteration_deduct_values != nil
+          cdv_iterations << PavementConditionIndex::CdvIteration.new(deduct_values: next_iteration_deduct_values, pavement_type: @pavement_type)
         end
+        cdv_iterations
       end
     end
 
-    # def distress_groups
-    #   @distress_groups ||= @distresses.group_by(&:type)
-    # end
+    def allowed_deduct_values
+      @allowed_deduct_values ||= begin
+        dvs = deduct_values.clone
+        adv = dvs.shift(allowable_number_of_deduct_values.floor)
+        adv << (allowable_number_of_deduct_values % 1) * dvs[0] if dvs.any?
+        adv
+      end
+    end
 
-    # def cdv_iterations
+    def allowable_number_of_deduct_values
+      @allowable_number_of_deducts ||= begin
+        m = 1 + ((9/98.0) * (100-highest_deduct_value))
+        [m,10.0].min
+      end
+    end
 
-    # end
+    def highest_deduct_value
+      @highest_deduct_value ||= deduct_values.max
+    end
 
-    # def highest_individual_deduct_value
-    #   @highest_deduct_value ||= @distresses.map(&:deduct_value).max || 0.0
-    # end
+    def deduct_values
+      @deduct_values ||= distress_groups.map(&:deduct_value).sort {|x,y| y <=> x}
+    end
 
-    # def allowable_number_of_deducts
+    def maximum_corrected_deduct_value
+      @maximum_corrected_deduct_value ||= cdv_iterations.map(&:corrected_deduct_value).max
+    end
 
-    # end
+    def pavement_condition_index
+      @pavement_condition_index ||= 100 - maximum_corrected_deduct_value
+    end
 
-    # def maximum_corrected_deduct_value
-
-    # end
-
-    # def pavement_condition_index
-    #   @pci ||= 100.0-maximum_corrected_deduct_value
-    # end
-
-    # def rating
-    #   @rating ||= PavementConditionIndex::RatingLookup.new(pci: pci).rating
-    # end
-
-
-
-
-
-    # def name
-    #   @room.number
-    # end
-
-    # def id
-    #   @room.id
-    # end
-
-    # def max_number_of_samples
-    #   @max_number_of_samples ||= distress_groups.map {|g| g[1].length}.inject(0) { |a,b| [a,b].max }
-    # end
-
-    # def distress_groups
-    #   @distress_groups ||= @room.distress_samples.group_by(&:distress_id)
-    # end
-
-    # def distresses
-    #   @distresses ||= all_distresses.delete_if {|dg| !dg.valid?}
-    # end
-
-    # def all_distresses
-    #   @all_distresses ||= distress_groups.map {|distress_group| ParkingLotReport::DistressGroup.new(distress_samples: distress_group[1], sample_area: @area)}
-    # end
-
-    # def invalid_distresses
-    #   @invalid_distresses ||= all_distresses.delete_if {|dg| dg.valid?}
-    # end
-
-
-
-    # def allowable_number_of_deducts
-    #   @allowable_number_of_deducts ||= begin
-    #     f = 1 + ((9/98.0) * (100-highest_deduct_value))
-    #     [f,10.0].min
-    #   end
-    # end
-
-    # def corrected_deduct_value_table
-    #   @corrected_deduct_value_table ||= ParkingLotReport::CorrectedDeductValueTable.new(allowable_number_of_deducts: allowable_number_of_deducts, deduct_values: distresses.map(&:deduct_value))
-    # end
-
-    # def maximum_corrected_deduct_value
-    #   @maximum_corrected_deduct_value ||= corrected_deduct_value_table.rows.map(&:cdv).max || 0
-    # end
+    def rating
+      @rating ||= PavementConditionIndex::PciRatingLookup.new(pavement_condition_index).rating
+    end
 
   end
 end
