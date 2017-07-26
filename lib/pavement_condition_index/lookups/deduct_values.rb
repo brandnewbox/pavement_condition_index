d53f8e9a61a823c7246e18706e2fe49f47f69202
Andy Monroe <jayhawker805@gmail.com>
2017-07-26 13:51:07 -0500

    Looking up deduct values at with an 'invalid' distress density now returns the same value as the nearest 'valid' distress density

--- lib/pavement_condition_index/lookups/deduct_values.rb
+++ lib/pavement_condition_index/lookups/deduct_values.rb
@@ -159,151 +159,149 @@ module PavementConditionIndex
             low: [-0.0002, 0.0042, 0.7952, -0.0078].reverse,
             medium: [-0.011, 1.9214, 0.0894].reverse,
             high: [0.0013, -0.1104, 4.4413, 19.0275].reverse,
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           },
           corner_break: {
             low: [0.0007, 0.8779, 0.0389].reverse,
             medium: [-0.0131, 1.6944, 0.0207].reverse,
             high: [0.0004, -0.0452, 2.788, 0.3587].reverse,
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           },
           divided_slab: {
             low: [-0.0032, 1.0373, -0.1269].reverse,
             medium: [0.0004, -0.0419, 2.3905, 0.2461].reverse,
             high: [0.0007, -0.0741, 3.6957, 0.452].reverse,
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           },
           durability_cracking: {
             low: [-0.0014, 0.3673, 0.0971].reverse,
             medium: [-0.0087, 1.0597, 0.057].reverse,
             high: [0.0004, -0.0472, 2.7205, 0.1412].reverse,
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           },
           faulting: {
             low: [-0.0003, 0.017, 0.1602, -0.2642].reverse,
             medium: [-0.0003, 0.0137, 0.5965, 0.1981].reverse,
             high: [-0.0086, 1.6102, 0.1839].reverse,
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           },
           joint_seal_damage: {
             low: [2.0],
             medium: [4.0],
             high: [8.0],
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           },
           lane_shoulder_drop_off: {
             low: [-0.0022, 0.1348, -0.0078].reverse,
             medium: [-0.0089, 0.6067, -0.0155].reverse,
             high: [-0.0113, 0.93, -0.0181].reverse,
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           },
           linear_cracking: {
             low: [-0.0048, 0.6151, -0.0363].reverse,
             medium: [-0.0112, 0.9263, -0.0466].reverse,
             high: [0.0005, -0.0455, 2.177, 0.4209].reverse,
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           },
           patching_large_and_utility_cuts: {
             low: [-0.0002, 0.0112, 0.1965, -0.2979].reverse,
             medium: [-0.0002, 0.0139, 0.4502, -0.0738].reverse,
             high: [0.0002, -0.0229, 1.8598, -0.0155].reverse,
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           },
           patching_small: {
             low: [0.0048, -0.016, -0.0945].reverse,
             medium: [0.0055, 0.1313, 0.0298].reverse,
             high: [-0.0054, 0.5558, -0.022].reverse,
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           },
           polished_aggregate: {
             low: [-0.0013, 0.2251, -0.0259].reverse,
             medium: [-0.0013, 0.2251, -0.0259].reverse,
             high: [-0.0013, 0.2251, -0.0259].reverse,
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           },
           popouts: {
             low: [-0.0002, 0.1967, 0.0945].reverse,
             medium: [-0.0002, 0.1967, 0.0945].reverse,
             high: [-0.0002, 0.1967, 0.0945].reverse,
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           },
           pumping: {
             low: [-0.0006, 0.6211, -0.0635].reverse,
             medium: [-0.0006, 0.6211, -0.0635].reverse,
             high: [-0.0006, 0.6211, -0.0635].reverse,
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           },
           punchouts: {
             low: [0.0002, -0.0225, 1.7159, 0.0466].reverse,
             medium: [0.0004, -0.0445, 2.6045, 0.1399].reverse,
             high: [0.0007, -0.0725, 3.5601, 0.4934].reverse,
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           },
           railroad_crossing: {
             low: [0.0002, -0.0175, 1.0867, -0.0032].reverse,
             medium: [0.0009, -0.0632, 2.432, -0.0597].reverse,
             high: [0.0058, -0.318, 7.9948, 0.3211].reverse,
-            valid_min: 0,
-            valid_max: 60
+            valid_min: 0.0,
+            valid_max: 60.0
           },
           scaling_map_cracking_crazing: {
             low: [0.0033, 0.1646, 0.0376].reverse,
             medium: [-0.0148, 0.9407, 0.0285].reverse,
             high: [0.0004, -0.0408, 2.0622, 0.3768].reverse,
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           },
           shrinkage_cracks: {
             low: [0.0063, -0.0835, 0.0596].reverse,
             medium: [0.0063, -0.0835, 0.0596].reverse,
             high: [0.0063, -0.0835, 0.0596].reverse,
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           },
           spalling_corner: {
             low: [0.0063, 0.074, -0.123].reverse,
             medium: [0.0044, 0.2576, -0.0557].reverse,
             high: [-0.0101, 0.7972, 0.0026].reverse,
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           },
           spalling_joint: {
             low: [-0.0011, 0.2552, 0.0013].reverse,
             medium: [-0.0005, 0.4821, -0.1153].reverse,
             high: [0.0003, -0.0324, 1.7875, 0.1904].reverse,
-            valid_min: 0,
-            valid_max: 100
+            valid_min: 0.0,
+            valid_max: 100.0
           }
         }
       }
 
       def call(value)
-        coefficients = DEDUCTS[@pavement_type][@distress_type][@severity]
+        valid_max_value = DEDUCTS[@pavement_type][@distress_type][:valid_max]
+        valid_min_value = DEDUCTS[@pavement_type][@distress_type][:valid_min]
+        value = valid_max_value if value > valid_max_value
+        value = valid_min_value if value < valid_min_value
         # Asphalt charts use logarithmic scale, concrete uses linear
         value = @pavement_type == :asphalt ? Math.log10(value.to_f) : value.to_f
+
+        coefficients = DEDUCTS[@pavement_type][@distress_type][@severity]
+
         result = generate_polynomial(*coefficients).call(value)
-        # NOTE: This is a strange limits thing, because we do not
-        # know what to do when a distress density goes "out of bounds"
-        # on those cool hand drawn charts. For example, in production
-        # we are seing a Bumps and Sags HIGH at 66% density, which is 
-        # "out of bounds" and returns a value of 176 using our 
-        # polynomials, which then generates a negative allowable_deducts
-        # value and that is kind of strange. So for now everything must land between
-        # 0 and 100. Love, - Nate and Andy, July 2017
         result = 100.0 if result > 100.0
         result = 0.0 if result < 0.0
         result
