56741460eacd8afa37ab13e49d411a7f947bc06f
Andy Monroe <jayhawker805@gmail.com>
2017-07-26 14:57:46 -0500

    Update section_condtion_survey

--- lib/pavement_condition_index/section_condition_survey.rb
+++ lib/pavement_condition_index/section_condition_survey.rb
@@ -1,37 +1,48 @@
 module PavementConditionIndex
   class SectionConditionSurvey
 
-    attr_reader :area, :sample_area, :sample_units
+    attr_reader :sample_unit_condition_surveys, :total_number_of_sample_units, :acceptable_error, :pci_standard_deviation
 
-    ACCEPTABLE_ERROR = 5
-    PCI_STANDARD_DEVIATION = 10
-
-    def initialize(area:nil,sample_area:nil,sample_units:nil)
-      @area = area
-      @sample_area = sample_area
-      @sample_units = sample_units
+    def initialize(sample_unit_condition_surveys:,total_number_of_sample_units:nil,acceptable_error:5,pci_standard_deviation:10)
+      @sample_unit_condition_surveys = sample_unit_condition_surveys
+      @total_number_of_sample_units = total_number_of_sample_units
+      @acceptable_error = acceptable_error
+      @pci_standard_deviation = pci_standard_deviation
+      @number_of_sample_units = @sample_unit_condition_surveys.length
     end
 
-    def required_sample_units
-      t = (total_number_of_sample_units * PCI_STANDARD_DEVIATION**2)
-      b = (ACCEPTABLE_ERROR**2/4) * (total_number_of_sample_units - 1) + PCI_STANDARD_DEVIATION**2
-      (t/b.to_f).ceil
+    def required_number_of_sample_unit_condition_surveys
+      @required_number_of_sample_unit_condition_surveys ||= begin
+        if @total_number_of_sample_units
+          t = (@total_number_of_sample_units * @pci_standard_deviation**2)
+          b = (@acceptable_error**2/4) * (@total_number_of_sample_units - 1) + @pci_standard_deviation**2
+          (t/b.to_f).ceil
+        else
+          1
+        end
+      end
     end
 
     def sample_is_statistically_significant
-      @number_of_sample_units >= required_sample_units
+      @sample_is_statistically_significant ||= @number_of_sample_units >= required_number_of_sample_unit_condition_surveys
     end
 
     def sample_spacing_interval
-      (number_of_sample_units/required_sample_units.to_f).floor
+      @sample_spacing_interval ||= (@number_of_sample_units/required_number_of_sample_unit_condition_surveys.to_f).floor
     end
 
-    def pavement_condition_invenetory
-      @pavement_condition_invenetory ||= begin
-        weighted_pci_scores = sample_units.map{|sample_unit| sample_unit.pavement_condition_invenetory * sample_unit.area}
-        weighted_pci_scores.sum / weighted_pci_scores.size.to_f
-      end
+    def weighted_pci_scores
+      @weighted_pci_scores ||= @sample_unit_condition_surveys.map{|sample_unit_condition_survey| sample_unit_condition_survey.pavement_condition_index * sample_unit_condition_survey.area}
     end
+
+    def sum_of_sample_unit_condition_survey_areas
+      @sum_of_sample_unit_condition_survey_areas ||= @sample_unit_condition_surveys.map{|sample_unit_condition_survey| sample_unit_condition_survey.area}.sum
+    end
+
+    def pavement_condition_index
+      @pavement_condition_index ||= weighted_pci_scores.sum / sum_of_sample_unit_condition_survey_areas.to_f
+    end
+    alias_method :pci, :pavement_condition_index
 
     def rating
       @rating ||= PavementConditionIndex::Lookups::PciRatings.new(pavement_condition_index).rating
---
+++ spec/section_condition_survey_spec.rb
@@ -0,0 +1,67 @@
+require "spec_helper"
+
+RSpec.describe PavementConditionIndex do
+  
+  describe 'concrete_sample_unit_condition_survey' do 
+    before do
+      @asphalt_sample_survey = PavementConditionIndex::SampleUnitConditionSurvey.new({
+        area: 2500, # Required: square footage of entire sample unit
+        pavement_type: :asphalt, # Required
+        distresses: [
+          {severity: :low, type: :alligator_cracking, quantity: 5},
+          {severity: :low, type: :alligator_cracking, quantity: 4},
+          {severity: :low, type: :alligator_cracking, quantity: 4},
+          {severity: :high, type: :alligator_cracking, quantity: 8},
+          {severity: :high, type: :alligator_cracking, quantity: 6},
+          {severity: :low, type: :edge_cracking, quantity: 32},
+          {severity: :low, type: :edge_cracking, quantity: 15},
+          {severity: :low, type: :edge_cracking, quantity: 18},
+          {severity: :low, type: :edge_cracking, quantity: 24},
+          {severity: :low, type: :edge_cracking, quantity: 41},
+          {severity: :medium, type: :joint_reflection_cracking, quantity: 20},
+          {severity: :medium, type: :joint_reflection_cracking, quantity: 15},
+          {severity: :medium, type: :joint_reflection_cracking, quantity: 35},
+          {severity: :medium, type: :joint_reflection_cracking, quantity: 27},
+          {severity: :medium, type: :joint_reflection_cracking, quantity: 23},
+          {severity: :medium, type: :joint_reflection_cracking, quantity: 10},
+          {severity: :medium, type: :joint_reflection_cracking, quantity: 13},
+          {severity: :high, type: :patching_and_utility_cut_patching, quantity: 12},
+          {severity: :high, type: :patching_and_utility_cut_patching, quantity: 10},
+          {severity: :low, type: :potholes, quantity: 1},
+          {severity: :low, type: :rutting, quantity: 4},
+          {severity: :low, type: :rutting, quantity: 9},
+          {severity: :low, type: :rutting, quantity: 8},
+          {severity: :low, type: :weathering, quantity: 250}
+        ]
+      })
+
+      @concrete_sample_survey = PavementConditionIndex::SampleUnitConditionSurvey.new({
+        area: 20, # Required: number of slabs
+        pavement_type: :concrete, # Required
+        distresses: [
+          {severity: :high, type: :joint_seal_damage, quantity: 123}, # joint_seal_damage has no area
+          {severity: :low, type: :corner_break, quantity: 3},
+          {severity: :medium, type: :corner_break, quantity: 1},
+          {severity: :medium, type: :divided_slab, quantity: 3},
+          {severity: :medium, type: :patching_small, quantity: 4},
+          {severity: :medium, type: :punchouts, quantity: 2},
+          {severity: :low, type: :spalling_corner, quantity: 6},
+          {severity: :high, type: :spalling_joint, quantity: 1},
+        ]
+      })
+
+      @section_survey = PavementConditionIndex::SectionConditionSurvey.new({
+        sample_unit_condition_surveys: [
+          @asphalt_sample_survey,
+          @concrete_sample_survey],
+        total_number_of_sample_units: 30
+        })
+    end
+
+    it 'has correct pci' do
+      expect(@section_survey.pci).to be_within(3).of(49)
+    end
+
+  end
+
+end
